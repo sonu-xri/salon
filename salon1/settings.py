f8bb7130cc556379c205905543534044b8fe67c5
Sonu  kumar <srai6644090@gmail.com>
2026-04-12 13:11:15 +0530

    update

--- salon1/settings.py
+++ salon1/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-3@0*680l(3*5lkx))18k)x@#&zc)j_+vb6vf30e=ht1r1a%*_j",
-)
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ.get(
+#     "DJANGO_SECRET_KEY",
+#     "django-insecure-3@0*680l(3*5lkx))18k)x@#&zc)j_+vb6vf30e=ht1r1a%*_j",
+# )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # On Render, default DEBUG to off unless DJANGO_DEBUG is explicitly enabled.
